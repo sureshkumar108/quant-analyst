0f2dadaeac3406b36c1aa178410905a59bf6ef9c
Sureshkumar Subburaman <sureshkumar108@gmail.com>
2026-03-13 18:22:26 -0400

    Initialize main.jsx with React application setup

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../quant_analyst.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
